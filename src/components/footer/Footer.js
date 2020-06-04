fc06059c69a6eaa14c3875a154310173a1cf3b19
ajost86 <alexander.jost@yahoo.de>
2020-06-04 15:37:16 +0200

    whatsapp

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -33,32 +33,25 @@ function Footer({ theme }) {
           <Column>
             <Logo src={LogoImg} alt="" />
             <Paragraph color="rgba(255, 255, 255, 0.7)">
-              58 Howard Street #2 San Francisco, CA 941
+              developer-lab.de
             </Paragraph>
-            <FooterLink to="http://google.com">contact@aereland.com</FooterLink>
+            <FooterLink to="mailto:hello@developer-lab.de">hello@developer-lab.de</FooterLink>
             <FooterLink to="/terms-and-conditions">AGB</FooterLink>
             <FooterLink to="/data-protections">Datenschutzerklärung</FooterLink>
             <FooterLink to="/contact">Impressum</FooterLink>
           </Column>
           <Column>
             <Heading6 color={theme.palette.neutral.white}>Mehr über uns</Heading6>
-            <FooterLink to="http://google.com">About us</FooterLink>
-            <FooterLink to="http://google.com">Before you go</FooterLink>
-            <FooterLink to="http://google.com">Online check in</FooterLink>
-            <FooterLink to="http://google.com">FAQ</FooterLink>
+            <FooterLink to="/case-studies">Case-Studies</FooterLink>
           </Column>
           <Column>
             <Heading6 color={theme.palette.neutral.white}>Quick links</Heading6>
-            <FooterLink to="http://google.com">Pick up locations</FooterLink>
-            <FooterLink to="/terms-and-conditions">Terms of Payment</FooterLink>
-            <FooterLink to="/terms-and-conditions">Privacy Policy</FooterLink>
-            <FooterLink to="http://google.com">Where to find us</FooterLink>
           </Column>
           <Column>
             <Tweet>
               <TweetBox>
-                <Paragraph color="inherit">&quot;Want to meet us?</Paragraph>
-                <TweetLink href="https://wa.me/491702988400?text=Hi%20there!">
+                <Paragraph color="inherit">Lass uns chatten!</Paragraph>
+                <TweetLink href="https://wa.me/491702988400?text=Hallo,%20ich%20würde%20gerne%20mehr%20über%20die%20Teamerweiterung%20von%20Developer%20Lab%20erfahren.">
                   Whatsapp Chat Starten
                 </TweetLink>
               </TweetBox>
@@ -76,7 +69,7 @@ function Footer({ theme }) {
       <Container>
         <Copyright>
           <Paragraph color="rgba(255, 255, 255, 0.7)">
-            © 2020 AeroLand. All Rights Reserved.
+            © 2020 developer-lab.de. All Rights Reserved.
           </Paragraph>
 
           <SocialIcons>
@@ -85,9 +78,6 @@ function Footer({ theme }) {
               href="https://www.facebook.com/Rent-A-Developer-108237200556773/"
             >
               <FacebookIcon width="18px" />
-            </SocialIcon>
-            <SocialIcon target="_blank" href="https://twitter.com">
-              <TwitterIcon width="18px" />
             </SocialIcon>
             <SocialIcon target="_blank" href="https://www.instagram.com/rent_a_developer/">
               <InstagramIcon width="18px" />
